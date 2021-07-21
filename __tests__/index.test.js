308c94096aa639329665f043bc83b77afe8c0abd
DJ Drakos <dianajodrakos@gmail.com>
2021-07-20 22:24:45 -0700

    created failing test for mapArray

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,6 +2,13 @@ import { mapArray } from '../index.js';
 
 describe('mapArray', () => {
   it('creates a new array with the results of calling a function on every item in the original array', () => {
+    const array = [1, 2, 3];
+    const callback = (item) => item + 1;
 
+    const expected = [2, 3, 4];
+    const actual = mapArray(array, callback);
+
+    expect(expected).toEqual(actual);
+    expect(array).not.toEqual(actual);
   });
 });
