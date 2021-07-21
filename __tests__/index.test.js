8010d005ddd08741a6e0f4f2cfa1994ea603fad3
DJ Drakos <dianajodrakos@gmail.com>
2021-07-20 22:35:10 -0700

    created failing test for filterArray

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,10 +4,10 @@ describe('Array Methods', () => {
 
   describe('mapArray', () => {
     it('creates a new array with the results of calling a function on every item in the original array', () => {
-      const array = [1, 2, 3];
+      const array = [1, 2, 3, 4];
       const callback = (item) => item + 1;
             
-      const expected = [2, 3, 4];
+      const expected = [2, 3, 4, 5];
       const actual = mapArray(array, callback);
             
       expect(expected).toEqual(actual);
@@ -17,7 +17,14 @@ describe('Array Methods', () => {
 
   describe('filterArray', () => {
     it('creates a new array of all items whose callback returns a truthy value', () => {
+      const array = [1, 2, 3, 4];
+      const callback = (item) => item % 2;
 
+      const expected = [2, 4];
+      const actual = filterArray(array, callback);
+
+      expect(expected).toEqual(actual);
+      expect(array).not.toEqual(actual);
     });
   });
 });
