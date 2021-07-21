9a342202e7c4c05f47ee052afa4ba46ab466e9ba
DJ Drakos <dianajodrakos@gmail.com>
2021-07-20 22:27:14 -0700

    mapArray test passes

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export const mapArray = (arr, callback) => {
-
+  const mappedArray = [];
+  for(let i = 0; i < arr.length; i++) {
+    mappedArray.push(callback(arr[i]));
+  }
+  return mappedArray;
 };
